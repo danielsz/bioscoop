05f92abac9a2d87d58072167a8b509b8fe3b51e4
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2025-08-06 13:08:41 +0800

    template for built-in filter

--- src/bioscoop/built_in.clj
+++ src/bioscoop/built_in.clj
@@ -5,29 +5,21 @@
    [clojure.spec.alpha :as s]
    [clojure.tools.logging :as log]))
 
-(defn crop [arg]
+
+(defn template [arg spec]
   (if (map? (first arg))
     (let [m (first arg)]
-      (if (s/valid? ::spec/crop m)
-        (make-filter "crop" m)
-        (s/explain-data ::spec/crop m)))
-    (let [formal-keys (last (s/form ::spec/crop))]
-      (make-filter "crop" (zipmap formal-keys arg)))))
+      (if (s/valid? spec m)
+        (make-filter (name spec) m)
+        (s/explain-data spec m)))
+    (let [formal-keys (last (s/form spec))]
+      (make-filter (name spec) (zipmap formal-keys arg)))))
+
+(defn crop [arg]
+  (template arg ::spec/crop))
 
 (defn scale [arg]
-  (if (map? (first arg))
-    (let [m (first arg)]
-      (if (s/valid? ::spec/scale m)
-        (make-filter "scale" m)
-        (s/explain-data ::spec/scale m)))
-    (let [formal-keys (last (s/form ::spec/scale))]
-      (make-filter "scale" (zipmap formal-keys arg)))))
+  (template arg ::spec/scale))
 
 (defn fade [arg]
-  (if (map? (first arg))
-    (let [m (first arg)]
-      (if (s/valid? ::spec/fade m)
-        (make-filter "fade" m)
-        (s/explain-data ::spec/fade m)))
-    (let [formal-keys (last (s/describe ::spec/fade))]
-      (make-filter "fade" (zipmap formal-keys arg)))))
+  (template arg ::spec/fade))
