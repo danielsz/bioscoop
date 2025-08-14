b07cfa3200e9faed73d75d0eb921fb5741dd8964
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2025-08-14 22:37:01 +0800

    FFmpeg parser tests

--- test/bioscoop/ffmpeg_parser_test.clj
+++ test/bioscoop/ffmpeg_parser_test.clj
@@ -63,38 +63,146 @@
 (deftest ffmpeg-grammar
   (testing "Filter with positional arguments"
     (let [expr "scale=640:480"]
-      (is (= expr expr))))
+      (is (= (ffmpeg/ffmpeg-parser expr)
+             [:filtergraph
+              [:filterchain
+               [:filter
+                [:filter-spec
+                 [:filter-name "scale"]
+                 [:filter-arguments [:unquoted-args "640:480"]]]]]]))))
   (testing "Labeled filter"
     (let [expr "scale@hd=1280:720"]
-      (is (= expr expr))))
+      (is (= (ffmpeg/ffmpeg-parser expr)
+             [:filtergraph
+              [:filterchain
+               [:filter
+                [:filter-spec
+                 [:filter-name "scale" [:at "@"] "hd"]
+                 [:filter-arguments [:unquoted-args "1280:720"]]]]]]))))
   (testing "Multiple output linklabels"
     (let [expr "[in]scale=1280:720[scaled1][scaled2]"]
-      (is (= expr expr)))
+      (is (= (ffmpeg/ffmpeg-parser expr)
+             [:filtergraph
+              [:filterchain
+               [:filter
+                [:input-linklabels [:linklabel "in"]]
+                [:filter-spec
+                 [:filter-name "scale"]
+                 [:filter-arguments [:unquoted-args "1280:720"]]]
+                [:output-linklabels
+                 [:linklabel "scaled1"]
+                 [:linklabel "scaled2"]]]]])))
     (let [expr "[0:v]split[o0][o1]"]
-      (is (= expr expr))))
+      (is (= (ffmpeg/ffmpeg-parser expr)
+             [:filtergraph
+              [:filterchain
+               [:filter
+                [:input-linklabels [:linklabel "0:v"]]
+                [:filter-spec [:filter-name "split"]]
+                [:output-linklabels [:linklabel "o0"] [:linklabel "o1"]]]]]))))
   (testing "Filter with instance label"
     (let [expr "[in]scale@highres=1920:1080[hd]"]
-      (is (= expr expr))))
+      (is (= (ffmpeg/ffmpeg-parser expr)
+             [:filtergraph
+              [:filterchain
+               [:filter
+                [:input-linklabels [:linklabel "in"]]
+                [:filter-spec
+                 [:filter-name "scale" [:at "@"] "highres"]
+                 [:filter-arguments [:unquoted-args "1920:1080"]]]
+                [:output-linklabels [:linklabel "hd"]]]]]))))
   (testing "Quoted string with escapes"
-    (let [expr "[v]drawtext=text='Hello\\'s World':fontsize=24[out]"]
-      (is (= expr expr)))
+    (let [expr "[v]drawtext=text='Hello\\\"s World':fontsize=24[out]"]
+      (is (= (ffmpeg/ffmpeg-parser expr)
+             [:filtergraph
+              [:filterchain
+               [:filter
+                [:input-linklabels [:linklabel "v"]]
+                [:filter-spec
+                 [:filter-name "drawtext"]
+                 [:filter-arguments
+                  [:unquoted-args "text="]
+                  [:quoted-string "Hello\\\"s World"]
+                  [:unquoted-args ":fontsize=24"]]]
+                [:output-linklabels [:linklabel "out"]]]]])))
     (let [expr "[v]drawtext=text='He said \\\"Hi\\\"':fontsize=24[out]"]
-      (is (= expr expr))))
+      (is (= (ffmpeg/ffmpeg-parser expr)
+             [:filtergraph
+              [:filterchain
+               [:filter
+                [:input-linklabels [:linklabel "v"]]
+                [:filter-spec
+                 [:filter-name "drawtext"]
+                 [:filter-arguments
+                  [:unquoted-args "text="]
+                  [:quoted-string "He said \\\"Hi\\\""]
+                  [:unquoted-args ":fontsize=24"]]]
+                [:output-linklabels [:linklabel "out"]]]]]))))
   (testing "Complex argument with special characters"
     (let [expr "[vid]drawtext=text='Price: $50\\%':x=(w-tw)/2:fontsize=24[out]"]
-      (is (= expr expr))))
+      (is (= (ffmpeg/ffmpeg-parser expr)
+             [:filtergraph
+              [:filterchain
+               [:filter
+                [:input-linklabels [:linklabel "vid"]]
+                [:filter-spec
+                 [:filter-name "drawtext"]
+                 [:filter-arguments
+                  [:unquoted-args "text="]
+                  [:quoted-string "Price: $50\\%"]
+                  [:unquoted-args ":x=(w-tw)/2:fontsize=24"]]]
+                [:output-linklabels [:linklabel "out"]]]]]))))
   (testing "Multiple filters in chain"
     (let [expr "[0:v]scale=640:360[small]; [1:a]volume=1.5[loud]"]
-      (is (= expr expr))))
+      (is (= (ffmpeg/ffmpeg-parser expr)
+             [:filtergraph
+              [:filterchain
+               [:filter
+                [:input-linklabels [:linklabel "0:v"]]
+                [:filter-spec
+                 [:filter-name "scale"]
+                 [:filter-arguments [:unquoted-args "640:360"]]]
+                [:output-linklabels [:linklabel "small"]]]]
+              [:filterchain
+               [:filter
+                [:input-linklabels [:linklabel "1:a"]]
+                [:filter-spec
+                 [:filter-name "volume"]
+                 [:filter-arguments [:unquoted-args "1.5"]]]
+                [:output-linklabels [:linklabel "loud"]]]]]))))
   (testing "Complex chain"
     (let [expr "[in]scale@thumb=320:240[small];[in]scale@full=1280:720[big]"]
-      (is (= expr expr))))
+      (is (= (ffmpeg/ffmpeg-parser expr)
+             [:filtergraph
+              [:filterchain
+               [:filter
+                [:input-linklabels [:linklabel "in"]]
+                [:filter-spec
+                 [:filter-name "scale" [:at "@"] "thumb"]
+                 [:filter-arguments [:unquoted-args "320:240"]]]
+                [:output-linklabels [:linklabel "small"]]]]
+              [:filterchain
+               [:filter
+                [:input-linklabels [:linklabel "in"]]
+                [:filter-spec
+                 [:filter-name "scale" [:at "@"] "full"]
+                 [:filter-arguments [:unquoted-args "1280:720"]]]
+                [:output-linklabels [:linklabel "big"]]]]]))))
   (testing "Sws_flags declaration"
     (let [expr "sws_flags=lanczos+accurate_rnd; [0:v]scale=1920:1080[hd]"]
-      (is (= expr expr))))
+      (is (= (ffmpeg/ffmpeg-parser expr)
+             [:filtergraph
+              [:sws-flags [:flags "lanczos+accurate_rnd"]]
+              [:filterchain
+               [:filter
+                [:input-linklabels [:linklabel "0:v"]]
+                [:filter-spec
+                 [:filter-name "scale"]
+                 [:filter-arguments [:unquoted-args "1920:1080"]]]
+                [:output-linklabels [:linklabel "hd"]]]]]))))
   (testing "Invalid case (empty linklabel)"
     (let [expr "[in]scale=640:480[]"]
-      (is (= expr expr)))))
+      (is (contains? (ffmpeg/ffmpeg-parser expr) :reason)))))
 
 
 
