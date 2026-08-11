4d7d38601a03621ca1cbaf16e54ebd08a75033de
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2026-08-11 16:55:06 +0800

    Bioscoop paper

--- paper/programming/auto/bioscoop_paper.el
+++ paper/programming/auto/bioscoop_paper.el
@@ -8,16 +8,16 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("fancyhdr" "") ("graphicx" "") ("amsmath" "") ("amssymb" "") ("amsfonts" "") ("algorithmic" "") ("textcomp" "") ("xcolor" "") ("listings" "") ("booktabs" "") ("multirow" "") ("ulem" "normalem") ("hyperref" "") ("biblatex" "backend=biber") ("csquotes" "") ("amsthm" "")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "programming"
@@ -26,7 +26,14 @@
     "csquotes"
     "amsthm")
    (LaTeX-add-labels
-    "tab:isomorphism")
+    "sec:motivating-example"
+    "fig:screenshot"
+    "sec:ast-convergence"
+    "tab:isomorphism"
+    "sec:limitations"
+    "sec:slideshow"
+    "tab:slideshow-scale"
+    "sec:music-video-code")
    (LaTeX-add-bibliographies
     "bioscoop")
    (LaTeX-add-amsthm-newtheorems
